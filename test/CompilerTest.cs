bf842d335ab62c83fba7edaf74d4098bacb8eb2b
Wenzel, Toni <toni.wenzel@chg-meridian.de>
2017-11-09 18:32:07 +0100

    Add test for imports

--- test/CompilerTest.cs
+++ test/CompilerTest.cs
@@ -51,5 +51,48 @@ namespace WebOptimizer.Dotless.Test
 
             Assert.Equal("* {\n  margin: 1px;\n}", result.AsString().Trim());
         }
+
+
+        [Fact]
+        public async Task Compile_With_Imports_Success()
+        {
+            var processor = new Compiler();
+            var pipeline = new Mock<IAssetPipeline>().SetupAllProperties();
+            var context = new Mock<IAssetContext>().SetupAllProperties();
+            var asset = new Mock<IAsset>().SetupAllProperties();
+            var env = new Mock<IHostingEnvironment>();
+            var fileProvider = new Mock<IFileProvider>();
+
+            string temp = Path.GetTempPath();
+            string path = Path.Combine(temp, "foo.less");
+            File.WriteAllText(path, "@import \"bar\"; @foo: 1px; * {margin: @foo}");
+
+            var secondPath = Path.Combine(temp, "bar.less");
+            File.WriteAllText(secondPath, ".test{ .active {color:red; }}");
+
+            var inputFile = new PhysicalFileInfo(new FileInfo(path));
+
+            context.Object.Content = new Dictionary<string, byte[]> {
+                { "/file.less", "@import \"bar\"; @foo: 1px; * {margin: @foo}".AsByteArray() },
+                 { "/bar.less", ".test{ .active {color:red; }}".AsByteArray() }
+            };
+
+            context.Setup(s => s.HttpContext.RequestServices.GetService(typeof(IHostingEnvironment)))
+                  .Returns(env.Object);
+
+            context.SetupGet(s => s.Asset)
+                        .Returns(asset.Object);
+
+            env.SetupGet(e => e.WebRootFileProvider)
+                 .Returns(fileProvider.Object);
+
+            fileProvider.Setup(f => f.GetFileInfo(It.IsAny<string>()))
+                   .Returns(inputFile);
+
+            await processor.ExecuteAsync(context.Object);
+            var result = context.Object.Content.First().Value;
+
+            Assert.Equal("* {\n  margin: 1px;\n}", result.AsString().Trim());
+        }
     }
 }
